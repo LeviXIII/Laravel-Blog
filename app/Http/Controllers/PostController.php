5a185b5f214adcb2a49c84237fbfa511a8f41ed3
LeviXIII <matthewthomas13@hotmail.com>
2018-01-31 18:08:48 -0500

    Added some protection to posts csrf.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -16,5 +17,31 @@ class PostController extends Controller
 
     public function create() {
         return view('posts/create');
+    }
+
+    public function store() {
+    
+        //Create a new post using request data.
+
+        /* This does the same thing as Post::create" */
+        
+        // $post = new Post;
+
+        // $post->title = request('title');
+        // $post->body = request('body');
+
+        // //Save it to the database.
+        // $post->save();
+
+        /*********************************************/
+
+        Post::create([
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+
+        //Redirect to the homepage.
+        return redirect('/');
+        
     }
 }
