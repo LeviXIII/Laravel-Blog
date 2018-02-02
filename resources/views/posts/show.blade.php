8abf97f6e4728b291b6441dc9f8ca564a556697a
LeviXIII <matthewthomas13@hotmail.com>
2018-02-02 16:11:40 -0500

    Styled comments. Still manually inputting.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,21 @@
     <h1>{{ $post->title }}</h1>
 
     <p>{{ $post->body }}</p>
+
+    <hr>
+
+    <section class="comments">
+      <ul class="list-group">
+        @foreach ($post->comments as $comment)
+          <li class="list-group-item">
+            <strong>
+              {{ $comment->created_at->diffForHumans() }}: &nbsp;
+            </strong>
+            {{ $comment->body }}
+          </li>
+        @endforeach
+      </ul>
+    </section>
   </section>
+
 @endsection
