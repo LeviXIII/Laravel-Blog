9b9bc21952ef8eb7eba809667cedef92bcb8cc9b
LeviXIII <matthewthomas13@hotmail.com>
2018-02-05 19:52:37 -0500

    Sorted and made archives dynamic.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+//use Carbon\Carbon;
 
 class PostController extends Controller
 {
@@ -12,9 +13,16 @@ class PostController extends Controller
 
     public function index() {
 
-        $posts = Post::latest()->get();
+        $posts = Post::latest()
+            ->filter(request(['month', 'year']))
+            ->get();
 
-        return view('posts/index', compact('posts'));
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')
+            ->orderByRaw('min(created_at) desc')
+            ->get()->toArray();
+        
+        return view('posts/index', compact('posts', 'archives'));
     }
 
     //The "Post $post" is taking the wildcard var. name and injecting
